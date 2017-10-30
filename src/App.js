70ab5abaa226943a88abc5bf16495ddbc30316d3
Jeff Mataya <jeff.mataya@gmail.com>
2017-10-30 11:30:01 -0400

    Very basic router wiring
    Add simple static routes that will let us access each page. In the
    future, we can parameterize the routes so that we have a more logical
    and linkable structure.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import HomePage from './pages/Home';
+import DetailsPage from './pages/Details';
+import ResultsPage from './pages/Results';
 
 class App extends Component {
   render() {
-    return <HomePage />;
+    return (
+      <Router>
+        <div>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/results" component={ResultsPage} />
+          <Route path="/details" component={DetailsPage} />
+        </div>
+      </Router>
+    )
   }
 }
 
